d452d580bc03ea9e2f6e5a62ed95988c982d1a65
WhiteBlackGoose <wbg@angouri.org>
2023-03-02 13:35:50 +0300

    minor change

--- init.lua
+++ init.lua
@@ -89,8 +89,7 @@ vim.cmd.colorscheme('darcula')
 
 
 -- Folding
-vim.o.foldcolumn = '1' -- '0' is not bad
-vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+vim.o.foldcolumn = '1'
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
 vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
